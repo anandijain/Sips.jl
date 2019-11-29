0198441aa4e18f23d14ee9e60f40e30e7e670404
anandijain <anandj@uchicago.edu>
2019-11-28 22:43:39 -0600

    running on gpu, semi decent print output + wonky plotting

--- src/DynamicalSips.jl
+++ src/DynamicalSips.jl
@@ -10,8 +10,10 @@ using CuArrays
 
 include("load.jl")
 include("utils.jl")
+include("vis.jl")
 using .LoadData
 using .SipsUtils
+using .SippyVision
 
 
 function main()
@@ -25,13 +27,16 @@ games = main()
 df = rand(games)
 len = size(df)[1]
 
+
 # add striding
-view_start = div(len, 4)
+view_start = div(len, 2)
 view_end = len
-stride_amt = 5
+stride_amt = 1
+
 
 # take subset of data
 subset = view(df, view_start:stride_amt:view_end, :)
+
 
 # convert moneylines into decimal
 subset[:, end-1:end] = map(SipsUtils.eq , subset[:, end-1:end])
@@ -40,57 +45,77 @@ subset[:, end-1:end] = map(SipsUtils.eq , subset[:, end-1:end])
 # first column
 t = copy(Array(subset[:, 1])) |> gpu
 
+
 # adjust offset to t0 = 0
-t -= t[0]
+t = (t .- t[1]) ./ 1000
+
 
 # get min max
-tspan = t[0], t[end]
-
+tspan = t[1], t[end]
 plot(t)
+
 
 # other columns
 data = copy(subset[:, 2:end]') |> gpu
 
+
 # first row
 u0 = data[:, 1] |> gpu
 
+
 # dimension of ode
 dim = length(u0)
+
 
 # neural net chain
 dudt = Chain(Dense(dim, 15, tanh)
             ,Dense(15, 40, tanh)
             ,Dense(40, dim)) |> gpu
 
-n_ode(x) = neural_ode(gpu(dudt), gpu(x), tspan, AutoTsit5(Rosenbrock23()), maxiters=1e7, saveat=t, reltol=1e-7, abstol=1e-9)
+n_ode(x) = neural_ode(gpu(dudt), gpu(x), tspan, AutoTsit5(Rosenbrock23()), maxiters=1e7, saveat=t, reltol=1e-6, abstol=1e-8)
+
 
 function predict_n_ode()
   n_ode(u0)
 end
 
-@time predicted = predict_n_ode()
 
 loss_n_ode() = sum(abs2,data .- predict_n_ode())
-
-example_loss = loss_n_ode()
-
 
 repeated_data = Iterators.repeated((), 1000)
 opt = ADAM(0.1)
 
-cb = function () #callback function to observe training
+cb = function ()
   display(loss_n_ode())
-  
-  # plot current prediction against data
   cur_pred = Flux.data(predict_n_ode())
-  display(cur_pred)
-  # display(size(cur_pred))
+  
+  pred_a_ml = cur_pred[end-1, :]
+  pred_h_ml = cur_pred[end, :]
+  
+  real_a_ml = data[end-1, :]
+  real_h_ml = data[end, :]
+
+  println("away predictions")
+  display(pred_a_ml[end-20:end]')
+  println("away reals")
+  display(real_a_ml[end-20:end]')
+  println("")
+
+  println("home predictions")
+  display(pred_h_ml[end-20:end]')
+  println("home real")
+  display(real_a_ml[end-20:end]')
+  
+  println("")
+  println("")
 
   pl = scatter(t, data[end-1:end, :]', label="data")
   scatter!(pl, t, cur_pred[end-1:end, :]', label="prediction")
-  yticks!([-5:1:5;])
-  xticks!(tspan[1]:1:tspan[2])
-  display(plot(pl, fig))
+  yticks!([-5:5;])
+  xticks!(t[1]:t[end])
+
+  pl = SippyVision.make_plot(t, data, cur_pred)
+  display(plot(pl, size=(900, 900)))
 end
 
 # Display the ODE with the initial parameter values.
